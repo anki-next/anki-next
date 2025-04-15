853ee4feed20e20fce551879ee6b929e9fc1b82d
ikkz <ylei.mk@gmail.com>
2025-04-15 15:08:16 +0800

    fix: disable dep check

--- packages/rpc-client/src/transport.ts
+++ packages/rpc-client/src/transport.ts
@@ -1,3 +1,26 @@
-export interface AnkiRpcTransport {
-  request(serviceId: number, methodId: number, payload: any): Promise<any>;
+import { RpcTransport } from '@protobuf-ts/runtime-rpc';
+
+export interface AnkiRpcTransport extends Pick<RpcTransport, 'mergeOptions'> {
+  request(
+    serviceId: number,
+    methodId: number,
+    payload: Uint8Array
+  ): Promise<Uint8Array>;
+}
+
+export function createRpcTransport(transport: AnkiRpcTransport): RpcTransport {
+  const notImplemented = () => {
+    throw new Error('not implemented');
+  };
+  return {
+    mergeOptions(options) {
+      return transport.mergeOptions(options);
+    },
+    unary(_method, _input, _options) {
+      throw new Error('todo');
+    },
+    serverStreaming: notImplemented,
+    clientStreaming: notImplemented,
+    duplex: notImplemented,
+  };
 }
